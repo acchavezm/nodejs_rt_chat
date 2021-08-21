d529ca0f6f181691ae505f248a6a25049d2bed97
Ángel Chávez <acchavezmoreno@gmail.com>
2021-08-21 17:22:46 -0500

    Implementacion inicial del chat

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.broadcast.emit('hi');
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        socket.broadcast.emit('bye');
+    });
 });
 
 server.listen(3000, () => {

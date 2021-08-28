35eeeb92b2071b4cf56155159f16ba2d4873d82c
Ángel Chávez <acchavezmoreno@gmail.com>
2021-08-28 00:01:11 -0500

    Avances en UI

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const path = require('path');
+var favicon = require('serve-favicon');
+
+var users_online = 0;
+
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
+app.use(express.static('public'));
+app.use(express.static('files'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -11,6 +21,8 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+    users_online += 1;
+    socket.emit('users_online',{'users_online': io.engine.clientsCount});
     socket.broadcast.emit('hi');
     socket.on('chat message', (msg) => {
         console.log('message: ' + msg);
@@ -18,6 +30,8 @@ io.on('connection', (socket) => {
     });
     socket.on('disconnect', () => {
         console.log('user disconnected');
+        users_online -= 1;
+        socket.emit('users_online',{'users_online': io.engine.clientsCount});
         socket.broadcast.emit('bye');
     });
 });
